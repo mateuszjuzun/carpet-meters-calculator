6ae061e37353ee3225f53d950b5361bea5740d05
Mateusz <mateusz.juzun@gmail.com>
2022-03-15 21:25:20 +0100

    Initial commit

---
+++ src/Carpet.java
@@ -0,0 +1,12 @@
+public class Carpet {
+    private double cost;
+
+    public Carpet(double cost){
+        if (cost>=0){this.cost=cost;} else
+            cost = 0;
+    }
+
+    public double getCost(){
+        return cost;
+    }
+}
